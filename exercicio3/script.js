371e5c7a590bd2a0d0e22b09406d73fed9dee04b
Nana-shi <leof.pereira2@gmail.com>
2022-08-29 13:51:54 -0300

    Fiz as 3 atividades refente a aula 007 variaveis

---
+++ exercicio3/script.js
@@ -0,0 +1,14 @@
+let variavelA = 10
+let variavelB = 25
+let variavelC
+
+console.log(variavelA)
+console.log(variavelB)
+console.log(variavelC)
+
+variavelC = variavelA
+variavelA = variavelB
+variavelB = variavelC
+
+console.log(variavelA)
+console.log(variavelB)
